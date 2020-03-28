1fa75b955fb1c77fb1f29c77569c9eb46b2dc7e9
She-Codes <n.y.shaw@gmail.com>
2020-03-28 13:26:00 -0400

    added Aylien API

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,20 @@
 var path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
+const aylienTextAPI = require('aylien_textapi');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 app.use(express.static('dist'));
 
 console.log(__dirname);
+
+var textapi = new aylienTextAPI({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
 app.get('/', function(req, res) {
   res.sendFile('dist/index.html');
